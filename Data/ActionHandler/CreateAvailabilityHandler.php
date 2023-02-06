c98e0481fff88fe1615a25ace2a061d0037258a4
Barry Brands <barrybrands02@hotmail.com>
2023-02-06 10:30:01 +0100

    Action name fix

---
+++ Data/ActionHandler/CreateAvailabilityHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CommonGateway\HuwelijksplannerBundle\ActionHandler;
+
+use CommonGateway\CoreBundle\ActionHandler\ActionHandlerInterface;
+use CommonGateway\HuwelijksplannerBundle\Service\CreateAvailabilityService;
+
+class CreateAvailabilityHandler implements ActionHandlerInterface
+{
+    private CreateAvailabilityService $createAvailabilityService;
+
+    public function __construct(CreateAvailabilityService $createAvailabilityService)
+    {
+        $this->createAvailabilityService = $createAvailabilityService;
+    }
+
+    /**
+     *  This function returns the requered configuration as a [json-schema](https://json-schema.org/) array.
+     *
+     * @throws array a [json-schema](https://json-schema.org/) that this  action should comply to
+     */
+    public function getConfiguration(): array
+    {
+        return [
+            '$id'         => 'https://vng.opencatalogi.nl/schemas/hp.availabilityCheck.schema.json',
+            '$schema'     => 'https://json-schema.org/draft/2020-12/schema',
+            'title'       => 'CreateAvailability',
+            'description' => 'This handler returns a welcoming string',
+            'required'    => [],
+            'properties'  => [],
+        ];
+    }
+
+    /**
+     * This function runs the createAvailability service.
+     *
+     * @param array $data          The data from the call
+     * @param array $configuration The configuration of the action
+     *
+     * @return array
+     */
+    public function run(array $data, array $configuration): array
+    {
+        return $this->createAvailabilityService->createAvailabilityHandler($data, $configuration);
+    }
+}
---
+++ Data/ActionHandler/HandleAssentHandler.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace CommonGateway\HuwelijksplannerBundle\ActionHandler;
+
+use CommonGateway\CoreBundle\ActionHandler\ActionHandlerInterface;
+use CommonGateway\HuwelijksplannerBundle\Service\HandleAssentService;
+
+class HandleAssentHandler implements ActionHandlerInterface
+{
+    private HandleAssentService $handleAssentService;
+
+    public function __construct(HandleAssentService $handleAssentService)
+    {
+        $this->handleAssentService = $handleAssentService;
+    }
+
+    /**
+     *  This function returns the requered configuration as a [json-schema](https://json-schema.org/) array.
+     *
+     * @throws array a [json-schema](https://json-schema.org/) that this  action schould comply to
+     */
+    public function getConfiguration(): array
+    {
+        return [
+            '$id'        => 'https://vng.opencatalogi.nl/schemas/hp.assent.schema.json',
+            '$schema'    => 'https://json-schema.org/draft/2020-12/schema',
+            'title'      => 'HandleAssent',
+            'required'   => ['huwelijksEntityId'],
+            'properties' => [
+                'huwelijksEntityId' => [
+                    'type'        => 'uuid',
+                    'description' => 'The id of the huwelijks entity',
+                    'example'     => 'b484ba0b-0fb7-4007-a303-1ead3ab48846',
+                    'nullable'    => true,
+                    '$ref'        => 'https://commongateway.huwelijksplanner.nl/schemas/hp.huwelijk.schema.json',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * This function runs the handleAssent function.
+     *
+     * @param array $data          The data from the call
+     * @param array $configuration The configuration of the action
+     *
+     * @return array
+     */
+    public function run(array $data, array $configuration): array
+    {
+        return $this->handleAssentService->handleAssentHandler($data, $configuration);
+    }
+}
