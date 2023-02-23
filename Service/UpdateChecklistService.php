edce6f164d977c90d16aef26e9fa1fe556b1825b
Sarai Misidjan <Sarai@conduction.nl>
2023-02-23 15:29:42 +0100

    updated UpdateChecklistService from codacy comments

--- Service/UpdateChecklistService.php
+++ Service/UpdateChecklistService.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  */
 class UpdateChecklistService
 {
+
     /**
      * @var EntityManagerInterface
      */
@@ -20,7 +21,7 @@ class UpdateChecklistService
     /**
      * @var SymfonyStyle
      */
-    private SymfonyStyle $io;
+    private SymfonyStyle $symfonyStyle;
 
     /**
      * @var array
@@ -32,8 +33,12 @@ class UpdateChecklistService
      */
     private array $configuration;
 
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    /**
+     * @param EntityManagerInterface $entityManager          The Entity Manager
+     */
+    public function __construct(
+        EntityManagerInterface $entityManager
+    ) {
         $this->entityManager = $entityManager;
         $this->data = [];
         $this->configuration = [];
@@ -42,13 +47,13 @@ class UpdateChecklistService
     /**
      * Set symfony style in order to output to the console.
      *
-     * @param SymfonyStyle $io
+     * @param SymfonyStyle $symfonyStyle
      *
      * @return self
      */
-    public function setStyle(SymfonyStyle $io): self
+    public function setStyle(SymfonyStyle $symfonyStyle): self
     {
-        $this->io = $io;
+        $this->symfonyStyle = $symfonyStyle;
 
         return $this;
     }
@@ -69,125 +74,251 @@ class UpdateChecklistService
         }//end if
 
         return $schema;
+
     }//end getSchema()
+
+    /**
+     * Checks the partners of the huwelijk
+     *
+     * @param ObjectEntity $huwelijk The huwelijk object
+     * @param array $checklist The checklist array
+     *
+     * @return ObjectEntity The huwelijk object with updated/created checklist
+     */
+    public function checkHuwelijkPartners(ObjectEntity $huwelijk, array $checklist): array
+    {
+        // Check partners.
+        $partnersCount = count($huwelijk->getValue('partners'));
+        if ($partnersCount < 2) {
+            $checklist['partners'] = [
+                'result'  => false,
+                'display' => 'Voor een huwelijk/partnerschap zijn minimaal 2 partners nodig',
+            ];
+
+            return $checklist;
+        }
+
+        if ($partnersCount > 2) {
+            $checklist['partners'] = [
+                'result'  => false,
+                'display' => 'Voor een huwelijk/partnerschap kunnen maximaal 2 partners worden opgegeven',
+            ];
+
+            return $checklist;
+        }
+
+        $checklist['partners'] = [
+            'result'  => true,
+            'display' => 'Partners zijn opgegeven',
+        ];
+
+        return $checklist;
+    }
+
+    /**
+     * Checks the witnesses of the huwelijk
+     *
+     * @param ObjectEntity $huwelijk The huwelijk object
+     * @param array $checklist The checklist array
+     *
+     * @return ObjectEntity The huwelijk object with updated/created checklist
+     */
+    public function checkHuwelijkWitnesses(ObjectEntity $huwelijk, array $checklist): array
+    {
+        // Check getuigen.
+        // @todo eigenlijk is het minimaal 1 en maximaal 2 getuigen per partner.
+        $witnessCount = count($huwelijk->getValue('getuigen'));
+        if ($witnessCount < 2) {
+            $checklist['getuigen'] = [
+                'result'  => false,
+                'display' => 'Voor een huwelijk/partnerschap zijn minimaal 2 getuigen nodig',
+            ];
+
+            return $checklist;
+        }
+
+        if ($witnessCount > 4) {
+            $checklist['getuigen'] = [
+                'result'  => false,
+                'display' => 'Voor een huwelijk/partnerschap kunnen maximaal 4 getuigen worden opgegeven',
+            ];
+
+            return $checklist;
+        }
+
+        $checklist['getuigen'] = [
+            'result'  => true,
+            'display' => 'Getuigen zijn opgegeven',
+        ];
+
+        return $checklist;
+    }
+
+    /**
+     * Checks the offeser of the huwelijk
+     *
+     * @param ObjectEntity $huwelijk The huwelijk object
+     * @param array $checklist The checklist array
+     *
+     * @return ObjectEntity The huwelijk object with updated/created checklist
+     */
+    public function checkHuwelijkOfficer(ObjectEntity $huwelijk, array $checklist): array
+    {
+        // Kijken naar ambtenaar.
+        if ($huwelijk->getValue('ambtenaar') === false) {
+            $checklist['ambtenaar'] = [
+                'result'  => false,
+                'display' => 'Nog geen ambtenaar opgegeven',
+            ];
+
+            return $checklist;
+        }
+
+        $checklist['ambtenaar'] = [
+            'result'  => true,
+            'display' => 'Ambtenaar is opgegeven',
+        ];
+
+        return $checklist;
+    }
+
+    /**
+     * Checks the moment of the huwelijk
+     *
+     * @param ObjectEntity $huwelijk The huwelijk object
+     * @param array $checklist The checklist array
+     *
+     * @return ObjectEntity The huwelijk object with updated/created checklist
+     */
+    public function checkHuwelijkMoment(ObjectEntity $huwelijk, array $checklist): array
+    {
+        // Kijken naar moment.
+        // @TODO trouwdatum minimaal 2 weken groter dan aanvraag datum.
+        if ($huwelijk->getValue('moment') === false) {
+            $checklist['moment'] = [
+                'result'  => false,
+                'display' => 'Nog geen moment opgegeven',
+            ];
+
+            return $checklist;
+        }
+
+        $checklist['moment'] = [
+            'result'  => true,
+            'display' => 'Moment is opgegeven',
+        ];
+
+        return $checklist;
+    }
+
+    /**
+     * Checks the products of the huwelijk
+     *
+     * @param ObjectEntity $huwelijk The huwelijk object
+     * @param array $checklist The checklist array
+     *
+     * @return ObjectEntity The huwelijk object with updated/created checklist
+     */
+    public function checkHuwelijkProducts(ObjectEntity $huwelijk, array $checklist): array
+    {
+        // Kijken naar producten.
+        $productsCount = count($huwelijk->getValue('producten'));
+        if ($productsCount === 0) {
+            $checklist['producten'] = [
+                'result'  => false,
+                'display' => 'Nog geen producten opgegeven',
+            ];
+
+            return $checklist;
+        }
+
+        $checklist['producten'] = [
+            'result'  => true,
+            'display' => 'Producten zijn opgegeven',
+        ];
+
+        return $checklist;
+    }
+
+    /**
+     * Checks the order of the huwelijk
+     *
+     * @param ObjectEntity $huwelijk The huwelijk object
+     * @param array $checklist The checklist array
+     *
+     * @return ObjectEntity The huwelijk object with updated/created checklist
+     */
+    public function checkHuwelijkOrder(ObjectEntity $huwelijk, array $checklist): array
+    {
+        // Kijken naar order.
+        if ($huwelijk->getValue('order') === false) {
+            $checklist['order'] = [
+                'result'  => false,
+                'display' => 'Nog geen order opgegeven',
+            ];
+
+            return $checklist;
+        }
+
+        $checklist['order'] = [
+            'result'  => true,
+            'display' => 'Order is opgegegeven',
+        ];
+
+        return $checklist;
+    }
+
+    /**
+     * Checks the case of the huwelijk
+     *
+     * @param ObjectEntity $huwelijk The huwelijk object
+     * @param array $checklist The checklist array
+     *
+     * @return ObjectEntity The huwelijk object with updated/created checklist
+     */
+    public function checkHuwelijkCase(ObjectEntity $huwelijk, array $checklist): array
+    {
+        // Kijken naar zaak.
+        if ($huwelijk->getValue('zaak') === false) {
+            $checklist['zaak'] = [
+                'result'  => false,
+                'display' => 'Nog geen zaak opgegeven',
+            ];
+
+            return $checklist;
+        }
+
+        $checklist['zaak'] = [
+            'result'  => true,
+            'display' => 'Zaak is opgegeven',
+        ];
+
+        return $checklist;
+    }
 
     /**
      * Checks data from the marriage object and updates the associated checklist.
      *
-     * @param ObjectEntity $huwelijk
+     * @param ObjectEntity $huwelijk The huwelijk object
      *
-     * @return ObjectEntity
+     * @return ObjectEntity The huwelijk object with updated/created checklist
      */
     public function checkHuwelijk(ObjectEntity $huwelijk): ObjectEntity
     {
-        if (!$checklistObject = $huwelijk->getValue('checklist')) {
+        if ($checklistObject = $huwelijk->getValue('checklist') === false) {
             $checklistSchema = $this->getSchema('https://huwelijksplanner.nl/schemas/hp.checklist.schema.json');
             $checklistObject = new ObjectEntity($checklistSchema);
         }
 
         $checklist = [];
 
-        // Check partners
-        if (count($huwelijk->getValue('partners')) < 2) {
-            $checklist['partners'] = [
-                'result'  => false,
-                'display' => 'Voor een huwelijk/partnerschap zijn minimaal 2 partners nodig',
-            ];
-        } elseif (count($huwelijk->getValue('partners')) > 2) {
-            $checklist['partners'] = [
-                'result'  => false,
-                'display' => 'Voor een huwelijk/partnerschap kunnen maximaal 2 partners worden opgegeven',
-            ];
-        } else {
-            $checklist['partners'] = [
-                'result'  => true,
-                'display' => 'Partners zijn opgegeven',
-            ];
-        }
-        // Check getuigen
-        // @todo eigenlijk is het minimaal 1 en maximaal 2 getuigen per partner
-        if (count($huwelijk->getValue('getuigen')) < 2) {
-            $checklist['getuigen'] = [
-                'result'  => false,
-                'display' => 'Voor een huwelijk/partnerschap zijn minimaal 2 getuigen nodig',
-            ];
-        } elseif (count($huwelijk->getValue('getuigen')) > 4) {
-            $checklist['getuigen'] = [
-                'result'  => false,
-                'display' => 'Voor een huwelijk/partnerschap kunnen maximaal 4 getuigen worden opgegeven',
-            ];
-        } else {
-            $checklist['getuigen'] = [
-                'result'  => true,
-                'display' => 'Getuigen zijn opgegeven',
-            ];
-        }
-
-        // Kijken naar ambtenaar
-        if (!$huwelijk->getValue('ambtenaar')) {
-            $checklist['ambtenaar'] = [
-                'result'  => false,
-                'display' => 'Nog geen ambtenaar opgegeven',
-            ];
-        } else {
-            $checklist['ambtenaar'] = [
-                'result'  => true,
-                'display' => 'Ambtenaar is opgegeven',
-            ];
-        }
-
-        // Kijken naar moment
-        // @TODO trouwdatum minimaal 2 weken groter dan aanvraag datum
-        if (!$huwelijk->getValue('moment')) {
-            $checklist['moment'] = [
-                'result'  => false,
-                'display' => 'Nog geen moment opgegeven',
-            ];
-        } else {
-            $checklist['moment'] = [
-                'result'  => true,
-                'display' => 'Moment is opgegeven',
-            ];
-        }
-
-        // Kijken naar producten
-        if (!count($huwelijk->getValue('producten')) > 1) {
-            $checklist['producten'] = [
-                'result'  => false,
-                'display' => 'Nog geen producten opgegeven',
-            ];
-        } else {
-            $checklist['producten'] = [
-                'result'  => true,
-                'display' => 'Producten zijn opgegeven',
-            ];
-        }
-
-        // Kijken naar order
-        if (!$huwelijk->getValue('order')) {
-            $checklist['order'] = [
-                'result'  => false,
-                'display' => 'Nog geen order opgegeven',
-            ];
-        } else {
-            $checklist['order'] = [
-                'result'  => true,
-                'display' => 'Order is opgegegeven',
-            ];
-        }
-
-        // Kijken naar zaak
-        if (!$huwelijk->getValue('zaak')) {
-            $checklist['zaak'] = [
-                'result'  => false,
-                'display' => 'Nog geen zaak opgegeven',
-            ];
-        } else {
-            $checklist['zaak'] = [
-                'result'  => true,
-                'display' => 'Zaak is opgegeven',
-            ];
-        }
+        $checklist = $this->checkHuwelijkPartners($huwelijk, $checklist);
+        $checklist = $this->checkHuwelijkWitnesses($huwelijk, $checklist);
+        $checklist = $this->checkHuwelijkOfficer($huwelijk, $checklist);
+        $checklist = $this->checkHuwelijkMoment($huwelijk, $checklist);
+        $checklist = $this->checkHuwelijkProducts($huwelijk, $checklist);
+        $checklist = $this->checkHuwelijkOrder($huwelijk, $checklist);
+        $checklist = $this->checkHuwelijkCase($huwelijk, $checklist);
 
         $checklistObject->hydrate($checklist);
         $this->entityManager->persist($checklistObject);
@@ -198,34 +329,5 @@ class UpdateChecklistService
         $this->entityManager->flush();
 
         return $huwelijk;
-    }
-
-    /**
-     * Checks data from the marriage request and updates the associated checklist.
-     *
-     * @param ?array $data
-     * @param ?array $configuration
-     *
-     * @throws LoaderError|RuntimeError|SyntaxError|TransportExceptionInterface
-     *
-     * @return array
-     */
-    public function updateChecklistHandler(?array $data = [], ?array $configuration = []): array
-    {
-        isset($this->io) && $this->io->success('updateChecklistHandler triggered');
-        $this->data = $data;
-        $this->configuration = $configuration;
-
-        // Check if the incommming data exisits and is a huwelijk object
-        // @TODO full refactor
-        // if (
-        //     in_array('id', $this->data) &&
-        //     $huwelijk = $this->objectEntityService->getObject(null, $this->data['id']) &&
-        //     $huwelijk->getEntity()->getName() == 'huwelijk'
-        // ) {
-        //     return $this->checkHuwelijk($huwelijk)->toArray();
-        // }
-
-        return $data;
-    }
+    }//end checkHuwelijk()
 }
