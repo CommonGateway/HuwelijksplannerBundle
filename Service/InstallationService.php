c3b62a2f0879da399b8e65b6fefb1bb6d7250e46
Sarai Misidjan <Sarai@conduction.nl>
2023-02-23 13:25:16 +0100

    added actions and updated installation service

--- Service/InstallationService.php
+++ Service/InstallationService.php
@@ -52,12 +52,6 @@ class InstallationService implements InstallerInterface
             'headers' => ['accept' => 'application/json'], 'auth' => 'apikey', 'apikey' => 'AccessKey !ChangeMe!', ],
     ];
 
-    public const ACTION_HANDLERS = [
-        ['name' => 'CreateAvailbility', 'actionHandler' => 'CommonGateway\HuwelijksplannerBundle\ActionHandler\CreateAvailabilityHandler', 'listens' => ['huwelijksplanner.calendar.listens'], 'conditions' => [[1 => 1]]],
-        ['name' => 'CreateMarriage', 'actionHandler' => 'CommonGateway\HuwelijksplannerBundle\ActionHandler\CreateMarriageHandler', 'listens' => ['huwelijksplanner.huwelijk.created']],
-        ['name' => 'UpdateChecklist', 'actionHandler' => 'CommonGateway\HuwelijksplannerBundle\ActionHandler\UpdateChecklistHandler', 'listens' => ['huwelijksplanner.default.listens'], 'conditions' => [[1 => 1]]],
-    ];
-
     public function __construct(EntityManagerInterface $entityManager, ContainerInterface $container)
     {
         $this->entityManager = $entityManager;
@@ -91,140 +85,6 @@ class InstallationService implements InstallerInterface
     public function uninstall()
     {
         // Do some cleanup
-    }
-
-    /**
-     * This function creates default configuration for the action.
-     *
-     * @param $actionHandler The actionHandler for witch the default configuration is set
-     *
-     * @return array
-     */
-    public function addActionConfiguration($actionHandler): array
-    {
-        $defaultConfig = [];
-
-        // What if there are no properties?
-        if (!isset($actionHandler->getConfiguration()['properties'])) {
-            return $defaultConfig;
-        }
-
-        foreach ($actionHandler->getConfiguration()['properties'] as $key => $value) {
-            switch ($value['type']) {
-                case 'string':
-                case 'array':
-                    if (key_exists('example', $value)) {
-                        $defaultConfig[$key] = $value['example'];
-                    }
-                    break;
-                case 'object':
-                    break;
-                case 'uuid':
-                    if (isset($value['$ref'])) {
-                        try {
-                            $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $value['$ref']]);
-                        } catch (Exception $exception) {
-                            throw new Exception("No entity found with reference {$value['$ref']}");
-                        }
-                        $defaultConfig[$key] = $entity->getId()->toString();
-                    }
-                    break;
-                default:
-                    return $defaultConfig;
-            }
-        }
-
-        return $defaultConfig;
-    }
-
-    /**
-     * Decides wether or not an array is associative.
-     *
-     * @param array $array The array to check
-     *
-     * @return bool Wether or not the array is associative
-     */
-    private function isAssociative(array $array): bool
-    {
-        if ([] === $array) {
-            return false;
-        }
-
-        return array_keys($array) !== range(0, count($array) - 1);
-    }
-
-    /**
-     * @param array $defaultConfig
-     * @param array $overrides
-     *
-     * @throws Exception
-     *
-     * @return array
-     */
-    public function overrideConfig(array $defaultConfig, array $overrides): array
-    {
-        foreach ($overrides as $key => $override) {
-            if (is_array($override) && $this->isAssociative($override)) {
-                $defaultConfig[$key] = $this->overrideConfig(isset($defaultConfig[$key]) ? $defaultConfig[$key] : [], $override);
-            } elseif ($key == 'entity') {
-                $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $override]);
-                if (!$entity) {
-                    throw new Exception("No entity found with reference {$override}");
-                }
-                $defaultConfig[$key] = $entity->getId()->toString();
-            } elseif ($key == 'source') {
-                $source = $this->entityManager->getRepository('App:Gateway')->findOneBy(['name' => $override]);
-                if (!$source) {
-                    throw new Exception("No source found with name {$override}");
-                }
-                $defaultConfig[$key] = $source->getId()->toString();
-            } else {
-                $defaultConfig[$key] = $override;
-            }
-        }
-
-        return $defaultConfig;
-    }
-
-    /**
-     * This function creates actions for all the actionHandlers in OpenCatalogi.
-     *
-     * @return void
-     */
-    public function addActions(): void
-    {
-        $actionHandlers = $this::ACTION_HANDLERS;
-        (isset($this->io) ? $this->io->writeln(['', '<info>Looking for actions</info>']) : '');
-
-        foreach ($actionHandlers as $handler) {
-            $actionHandler = $this->container->get($handler['actionHandler']);
-
-            if (array_key_exists('name', $handler)) {
-                if ($this->entityManager->getRepository('App:Action')->findOneBy(['name' => $handler['name']])) {
-                    (isset($this->io) ? $this->io->writeln(['Action found with name '.$handler['name']]) : '');
-                    continue;
-                }
-            } elseif ($this->entityManager->getRepository('App:Action')->findOneBy(['class' => get_class($actionHandler)])) {
-                (isset($this->io) ? $this->io->writeln(['Action found for '.$handler['actionHandler']]) : '');
-                continue;
-            }
-
-            if (!$actionHandler->getConfiguration()) {
-                continue;
-            }
-
-            $defaultConfig = $this->addActionConfiguration($actionHandler);
-            isset($handler['config']) && $defaultConfig = $this->overrideConfig($defaultConfig, $handler['config']);
-
-            $action = new Action($actionHandler);
-            array_key_exists('name', $handler) ? $action->setName($handler['name']) : '';
-            $action->setListens($handler['listens'] ?? ['huwelijksplanner.default.listens']);
-            $action->setConfiguration($defaultConfig);
-            $action->setConditions($handler['conditions'] ?? ['==' => [1, 1]]);
-
-            $this->entityManager->persist($action);
-            (isset($this->io) ? $this->io->writeln(['Created Action '.$action->getName().' with Handler: '.$handler['actionHandler']]) : '');
-        }
     }
 
     /**
@@ -453,9 +313,6 @@ class InstallationService implements InstallerInterface
 
         // create cronjobs
         $this->createCronjobs();
-
-        // create actions from the given actionHandlers
-        $this->addActions();
 
         $this->entityManager->flush();
     }
